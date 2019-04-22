104719d80ba443a6e6580ca3d66185348f00e13a
Aaron Neville <aaron@designliftoff.com>
2019-04-22 12:04:34 -0700

    config

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,14 +60,11 @@ const Navbar = class extends React.Component {
               <Link className='navbar-item' to='/about'>
                 About
               </Link>
-              <Link className='navbar-item' to='/products'>
-                Products
+              <Link className='navbar-item' to='/classes'>
+                Classes
               </Link>
               <Link className='navbar-item' to='/contact'>
                 Contact
-              </Link>
-              <Link className='navbar-item' to='/contact/examples'>
-                Form Examples
               </Link>
             </div>
           </div>
