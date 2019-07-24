4312d5da5fab95d1f14d4d86fe7b3ce00a15dcdd
Aaron Neville <aaron@designliftoff.com>
2019-07-23 22:30:54 -0700

    banner

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import React from "react";
+import Helmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
 
-import SiteNavbar from '../components/Navbar';
+import SiteNavbar from "../components/Navbar";
 
-import './all.scss';
+import "../scss/all.scss";
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
@@ -21,42 +21,42 @@ const TemplateWrapper = ({ children }) => (
     render={data => (
       <div>
         <Helmet>
-          <html lang='en' className='has-navbar-fixed-top' />
+          <html lang="en" className="has-navbar-fixed-top" />
           <title>{data.site.siteMetadata.title}</title>
           <meta
-            name='description'
+            name="description"
             content={data.site.siteMetadata.description}
           />
 
           <link
-            rel='apple-touch-icon'
-            sizes='180x180'
-            href='/img/apple-touch-icon.png'
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/apple-touch-icon.png"
           />
           <link
-            rel='icon'
-            type='image/png'
-            href='/img/favicon-32x32.png'
-            sizes='32x32'
+            rel="icon"
+            type="image/png"
+            href="/img/favicon-32x32.png"
+            sizes="32x32"
           />
           <link
-            rel='icon'
-            type='image/png'
-            href='/img/favicon-16x16.png'
-            sizes='16x16'
+            rel="icon"
+            type="image/png"
+            href="/img/favicon-16x16.png"
+            sizes="16x16"
           />
 
           <link
-            rel='mask-icon'
-            href='/img/safari-pinned-tab.svg'
-            color='#ff4400'
+            rel="mask-icon"
+            href="/img/safari-pinned-tab.svg"
+            color="#ff4400"
           />
-          <meta name='theme-color' content='#fff' />
+          <meta name="theme-color" content="#fff" />
 
-          <meta property='og:type' content='business.business' />
-          <meta property='og:title' content={data.site.siteMetadata.title} />
-          <meta property='og:url' content='/' />
-          <meta property='og:image' content='/img/og-image.jpg' />
+          <meta property="og:type" content="business.business" />
+          <meta property="og:title" content={data.site.siteMetadata.title} />
+          <meta property="og:url" content="/" />
+          <meta property="og:image" content="/img/og-image.jpg" />
         </Helmet>
         <SiteNavbar />
         <div>{children}</div>
